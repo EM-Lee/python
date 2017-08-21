2c7c42ddeee87884ba62bdc05c11f9c55684cb41
EM-Lee <ten4han@gmail.com>
2017-08-21 09:55:21 -0400

    Add files via upload

---
+++ beginning/200 codes/061.py
@@ -0,0 +1,13 @@
+numdata = 57
+strdata = 'python'
+listdata = [1, 2, 3]
+dictdata = {'a':1, 'b':2}
+
+def func():
+    print('hi')
+
+print(type(numdata))
+print(type(strdata))
+print(type(listdata))
+print(type(dictdata))
+print(type(func))
